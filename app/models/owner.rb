5dadaf0d41b83761220c0b69edcb8931339148ba
Michael Aldaba <maldaba3@gmail.com>
2015-10-30 15:20:05 -0400

    Use Rails logger tool

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -9,6 +9,8 @@ class Owner
 	end
 
 	def countdown
+		Rails.logger.debug 'DEBUG: entering Owner countdown method'
+		Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
 		today = Date.today
 		birthday = Date.new(today.year, birthdate.month, birthdate.day)
 		if birthday > today
@@ -16,5 +18,6 @@ class Owner
 		else
 			countdown = (birthday.next_year - today).to_i
 		end
+
 	end
 end
